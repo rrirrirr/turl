d1a2a2e7244111736baae163e3d5e0ec297d25eb
rrirrirr <drpihl@gmail.com>
2023-01-28 12:54:08 +0100

    add styling

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,3 +1,5 @@
+import { Button, Container, TextInput } from '@mantine/core'
+import { useForm } from '@mantine/form'
 import axios from 'axios'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
@@ -5,31 +7,55 @@ import { useRouter } from 'next/router'
 export default function Signup() {
   const router = useRouter()
   const { data: session, status } = useSession()
+  const form = useForm({
+    initialValues: {
+      username: '',
+      firstName: '',
+      lastName: '',
+      telephoneNum: '',
+      password: '',
+    },
+
+    validate: {},
+  })
 
   const user = (session?.user as AuthUser) || null
   if (session) {
     router.push(`/user/${user.userId}`)
   }
 
-  async function handleSubmit(event: any): Promise<void> {
-    event.preventDefault()
+  interface UserS {
+    username: string
+    firstName: string
+    lastName: string
+    telephoneNum: string
+    password: string
+  }
+
+  async function handleSubmit({
+    username,
+    firstName,
+    lastName,
+    telephoneNum,
+    password,
+  }: UserS): Promise<void> {
     try {
       const userRes = await axios.post(
         `${process.env.NEXT_PUBLIC_DB_HOST}/users`,
         {
-          first_name: event.target.firstName.value,
-          last_name: event.target.lastName.value,
+          first_name: firstName,
+          last_name: lastName,
           isAdmin: false,
-          email: event.target.email.value,
-          password: event.target.password.value,
-          telephone_num: event.target.telephoneNum.value || null,
+          email: username,
+          password: password,
+          telephone_num: telephoneNum,
         }
       )
       if (userRes.status === 201) {
         const user = userRes.data
         const loginRes = await signIn('credentials', {
-          username: user.email,
-          password: event.target.password.value,
+          username: username,
+          password: password,
         })
         router.push(`/user/${user.id}`)
       }
@@ -39,27 +65,43 @@ export default function Signup() {
   }
 
   return (
-    <>
-      <section>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-mail</label>
-          <input type="text" id="email" name="email" required />
+    <section>
+      <Container size="xs">
+        <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+          <TextInput
+            label="Email"
+            type="text"
+            {...form.getInputProps('username')}
+            required
+          />
+          <TextInput
+            withAsterisk
+            label="Förnamn"
+            required
+            {...form.getInputProps('firstName')}
+          />
+          <TextInput
+            withAsterisk
+            required
+            label="Efternamn"
+            {...form.getInputProps('lastName')}
+          />
+          <TextInput
+            label="Telefonnummer"
+            {...form.getInputProps('telephoneNum')}
+          />
+          <TextInput
+            label="Lösenord"
+            type="text"
+            {...form.getInputProps('password')}
+            required
+          />
 
-          <label htmlFor="firstName">Förnamn</label>
-          <input type="text" id="firstName" name="firstName" required />
-
-          <label htmlFor="lastName">Efternamn</label>
-          <input type="text" id="lastName" name="lastName" required />
-
-          <label htmlFor="name">Telefonnummer (optional)</label>
-          <input type="text" id="telephoneNum" name="telephoneNum" />
-
-          <label htmlFor="password">Lösenord</label>
-          <input type="text" id="password" name="password" required />
-
-          <button type="submit">Skapa</button>
+          <Button color="red" type="submit" my="sm">
+            Skapa
+          </Button>
         </form>
-      </section>
-    </>
+      </Container>
+    </section>
   )
 }
